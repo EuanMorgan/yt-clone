cc65688fc238eb7b9cafbcad10a4d29374730ad9
ck-euan <euan.morgan@countingking.co.uk>
2025-02-12 22:19:07 +0000

    chore: @tailwindcss/upgrade

--- src/modules/home/ui/components/home-navbar/search-input.tsx
+++ src/modules/home/ui/components/home-navbar/search-input.tsx
@@ -8,7 +8,7 @@ export const SearchInput = () => {
         <input
           type="text"
           placeholder="Search"
-          className="w-full pl-4 py-2 pr-12 rounded-l-full border focus:outline-none focus:border-blue-500"
+          className="w-full pl-4 py-2 pr-12 rounded-l-full border focus:outline-hidden focus:border-blue-500"
         />
         {/* todo: add remove search btn */}
       </div>
